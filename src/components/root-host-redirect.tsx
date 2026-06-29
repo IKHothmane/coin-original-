17fe81213ea52783bc0dd4fa80eb470de494fec1
IKHothmane <ikhlef.othmane@gmail.com>
2026-06-29 01:32:25 +0100

    car4

--- src/components/root-host-redirect.tsx
+++ src/components/root-host-redirect.tsx
@@ -3,12 +3,12 @@
 import { useEffect } from "react";
 
 const CAR_SUBDOMAIN = "car.coinoriginal.shop";
-const CAR_TARGET_URL = "https://coinoriginal.shop/car";
+const CAR_TARGET_PATH = "/car";
 
 export function RootHostRedirect() {
   useEffect(() => {
-    if (window.location.hostname === CAR_SUBDOMAIN) {
-      window.location.replace(CAR_TARGET_URL);
+    if (window.location.hostname === CAR_SUBDOMAIN && window.location.pathname === "/") {
+      window.location.replace(`${window.location.origin}${CAR_TARGET_PATH}`);
     }
   }, []);
 
