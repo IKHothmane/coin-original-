3fde0e33251508c00a337170f1d748483bb64227
IKHothmane <ikhlef.othmane@gmail.com>
2026-06-29 01:21:06 +0100

    car1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-import { headers } from "next/headers";
-import { CarLandingPage } from "@/components/car-landing/car-landing-page";
 import { Homepage } from "@/components/homepage";
-
-const CAR_SUBDOMAIN = "car.coinoriginal.shop";
-
-export default async function Home() {
-  const requestHeaders = await headers();
-  const hostHeader = requestHeaders.get("x-forwarded-host") ?? requestHeaders.get("host") ?? "";
-  const host = hostHeader.split(":")[0].toLowerCase();
-
-  if (host === CAR_SUBDOMAIN) {
-    return <CarLandingPage />;
-  }
-
+export default function Home() {
   return <Homepage />;
 }
